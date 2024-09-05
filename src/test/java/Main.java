2c010994d6ad2cfa84d178715348fc3b0b10c460
Test DG User <Test DG Email>
2024-09-05 23:41:48 +0300

    Refactor in Main

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+    private static String msg = "Hello, world!";
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(getMessage());
+    }
+
+    public static String getMessage() {
+        return msg;
     }
 }
